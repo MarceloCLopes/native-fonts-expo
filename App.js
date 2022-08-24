2e70cf9a97e6a069d3127f77326c89dedafde4c1
MarceloCLopes <marc01mcl@gmail.com>
2022-08-24 17:35:06 -0300

    use fonts google expo

--- App.js
+++ App.js
@@ -1,11 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect } from "react";
+import { Inter_900Black } from "@expo-google-fonts/inter";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_900Black,
+    Ultra: require("./assets/fonts/Ultra-Regular.ttf"),
+    Dyna: require("./assets/fonts/DynaPuff.ttf"),
+    Rubik: require("./assets/fonts/RubikMazeRegular.ttf"),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayout}>
       <StatusBar style="auto" />
+      <Text
+        style={{
+          fontFamily: "Ultra",
+          fontSize: 25,
+        }}
+      >
+        Code with Marcelo!
+      </Text>
+      <Text
+        style={{
+          fontFamily: "Dyna",
+          fontSize: 25,
+        }}
+      >
+        Code with Marcelo!
+      </Text>
+      <Text
+        style={{
+          fontFamily: "Rubik",
+          fontSize: 25,
+        }}
+      >
+        Code with Marcelo!
+      </Text>
+      <Text
+        style={{
+          fontFamily: "Inter_900Black",
+          fontSize: 25,
+        }}
+      >
+        Code with Marcelo!
+      </Text>
     </View>
   );
 }
@@ -13,8 +70,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#d4d4d4",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,18 @@
+# Fontes
+
+Exemplo de uso de fonts em react native. Usando fontes no próprio app quanto expo.
+
+#### tecnologias usadas:
+
+- expo-splash-screen
+- @expo-google-fonts/inter
+
+---
+
+## Screenshot
+
+<div>
+  <img src=".github/img1.png" style="width:290px"/>
+</div>
+
+---
